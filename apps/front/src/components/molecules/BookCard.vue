788b2db9049a36a509360ab6ab0130e65a160ec9
Anthony HAREL <a.harel@polysoude.com>
2024-12-13 14:25:08 +0100

    Mode clair/sombre : améliorations d'affichage au niveau du background

--- apps/front/src/components/molecules/BookCard.vue
+++ apps/front/src/components/molecules/BookCard.vue
@@ -7,7 +7,7 @@ const { book } = defineProps<{ book: Book }>()
 
 <template>
   <div
-    class="bg-gray-200 border border-gray-800 rounded-lg p-4 space-y-2 hover:scale-105 hover:rotate-3 transition-all duration-200"
+    class="bg-gray-200 border border-gray-800 rounded-lg p-4 space-y-2 hover:scale-105 hover:border-green-400 hover:border-4 transition-all duration-200"
   >
     <h2 class="poly-LgTextBold">{{ book.title }}</h2>
     <p>{{ book.author }}</p>
